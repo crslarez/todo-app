da9d7aebac1ccf360ef01f72e250ae0e606d8a65
Carlos Salazar <crslarez@gmail.com>
2023-03-04 01:52:20 -0500

    Feat: t: TodoInput

---
+++ src/components/TodoInput/TodoInput.jsx
@@ -0,0 +1,14 @@
+const TodoInput = () => {
+    return (
+        <div className="mt-6 relative">
+            <div className="absolute inset-y-0 left-0 pl-3 fle flex items-center pointer-events-none">
+                <span className="border border-gray-500 border-solid p-3 rounded-full "></span>
+
+            </div>
+
+        </div>
+    )
+
+}
+
+export {TodoInput}
