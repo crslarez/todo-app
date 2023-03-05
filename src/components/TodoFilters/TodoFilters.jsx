7680ba6564aaf24c8be24878b3de085689b2391e
Carlos Salazar <crslarez@gmail.com>
2023-03-05 01:20:12 -0500

    pum

--- src/components/TodoFilters/TodoFilters.jsx
+++ src/components/TodoFilters/TodoFilters.jsx
@@ -1,8 +1,20 @@
+import { FilterButtom, FilterButtomContainer, FiltersContainer, ItemsLeft } from "./TodoFilters.components"
+
 const TodoFilters = () => {
     return (
-        <div>
-            
-        </div>
+        <FiltersContainer>
+            <ItemsLeft />
+            <FilterButtomContainer>
+                <FilterButtom action={() => { }} active="All" filter='All' />
+                <FilterButtom action={() => { }} active="All" filter='Active' />
+                <FilterButtom action={() => { }} active="All" filter='Completed' />
+            </FilterButtomContainer>
+
+        <button className="text-gray-400 hover:text-white cursor-pointer transition-all duration-300 ease-in">
+            Clear Completed
+        </button>
+        </FiltersContainer>
+
 
     )
 }
