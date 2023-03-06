1a18be00a3dcaca6199e827500162cd72abe8da4
Carlos Salazar <crslarez@gmail.com>
2023-03-06 00:36:21 -0500

    Feat: logic

--- src/App.js
+++ src/App.js
@@ -27,8 +27,9 @@ function App() {
       title: 'Study 2 hours',
       completed : false,
     }
-
   ])
+
+
   return (
     <div className="bg-gray-900 min-h-screen h-full font-inter text-gray-100 flex items-center justify-center py-20 px-5">
       <div className="container flex flex-col max-w-xl">
