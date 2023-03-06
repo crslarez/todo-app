cd61c42ea66e248f7d7f2f3921d836b3353f3542
Carlos Salazar <crslarez@gmail.com>
2023-03-06 00:19:13 -0500

    Feat: todos

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
+import { useState } from "react";
 import { Title } from "./components/Title/";
 import { Todo } from "./components/Todo";
 import { TodoInput } from "./components/TodoInput";
 import { TodoList } from "./components/TodoList";
 
 function App() {
+
+  const [todos, setTodos] = useState([
+    {
+      id:1,
+      title: 'Watch the next Marvel Movie',
+      completed: false,
+    },
+    {
+      id:2,
+      title: 'Record the next video',
+      completed: false,
+    },
+    {
+      id:3,
+      title: 'Whash the dishes',
+      completed: false,
+    },
+    {
+      id:4,
+      title: 'Study 2 hours',
+      completed : false,
+    }
+
+  ])
   return (
     <div className="bg-gray-900 min-h-screen h-full font-inter text-gray-100 flex items-center justify-center py-20 px-5">
       <div className="container flex flex-col max-w-xl">
         <Title />
         <TodoInput />
-        <TodoList>
-          <Todo />
-          <Todo />
-          <Todo />
-          <Todo />
-          <Todo />
-          <Todo />
-        </TodoList>
+        <TodoList todos={todos} />
+
       </div>
     </div>
   );
--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,10 +1,10 @@
-const Todo = () => { 
+const Todo = ({ todo }) => { 
     return(
         <div className="flex items-center justify-between p-4 bg-gray-700 border-b border-solid border-gray-600">
             <div className="flex items-center">
                 <span className="border-solid border border-gray-500 rounded-full p-3 cursor-pointer"></span>
             <p className="pl-3">
-                Todo Item
+                {todo.title}
             </p>
             </div>
             <img className="h-5 w-5 cursor-pointer transition-all duration-300 easy-in" src="/close-icon.svg" alt="Close Icon"/>
