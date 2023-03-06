185c92c4e25e52cb0b35a08d8caab1519ed8408d
Carlos Salazar <crslarez@gmail.com>
2023-03-05 23:22:03 -0500

    Feat: add TodoFilters

--- src/components/TodoFilters/TodoFilters.jsx
+++ src/components/TodoFilters/TodoFilters.jsx
@@ -1,8 +1,13 @@
-import { FiltersContainer, ItemsLeft } from "./TodoFilters.components"
+import { FilterButton, FilterButtonContainer, FiltersContainer, ItemsLeft } from "./TodoFilters.components"
 const TodoFilters = () => {
     return (
         <FiltersContainer>
             <ItemsLeft />
+            <FilterButtonContainer>
+                <FilterButton action={()=> {}} active="All" filter='All' />
+                <FilterButton action={()=> {}} active="All" filter='Active' />
+                <FilterButton action={()=> {}} active="All" filter='Completed' />    
+            </FilterButtonContainer>
         </FiltersContainer>
 
     )
